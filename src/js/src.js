6435060a264ef9772860f79d12be83b54eb835e0
Bamtop <pablobenitez01_@gmail.com>
2023-02-24 13:54:02 +0100

    Implement Debounce and search by name

--- src/js/src.js
+++ src/js/src.js
@@ -2,6 +2,10 @@
 const browser = document.getElementById("search");
 const content = document.getElementsByClassName('content')[0];
 const button = document.getElementsByClassName("search-button")[0];
+const searchInput = document.getElementById("search");
+const processChange = debounce((value) =>{
+    fetchDataAndCreateCard();
+});
 const fetchDataAndCreateCard = () => {
         while (content.hasChildNodes()){
         content.removeChild(content.lastChild);
@@ -14,20 +18,38 @@ const fetchDataAndCreateCard = () => {
 
 const create = (tableData) => {
     tableData['results'].forEach(data => {
-        const dataRow = createCard(data);
-        content.appendChild(dataRow);
+        const main = document.querySelector("content");
+        const card = document.createRange().createContextualFragment(/*html*/`
+   <div class="card">
+    <img src=${data.image} alt="Avatar" style="width:100%">
+       <div class="container">
+        <h4><b>${data.name}</b></h4>
+        <p>${data.status}</p>
+        <p>${data.species}</p>
+        <p>${data.type}</p>
+        
+        </div>
+    </div>
+    
+    `)
+        content.append(card);
     });
 
+
+
 }
 
-const createCard = (rowData) => {
-
-
-    // imagen
-    const imageTd = document.createElement('img');
-    imageTd.setAttribute('src',rowData['image']);
-
-    return imageTd;
+function debounce(func, wait = 300) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
 }
+searchInput.addEventListener("keyup", (e) => {
+    processChange(e.target.value);
+});
+
+
 
 button.addEventListener('click', fetchDataAndCreateCard);
